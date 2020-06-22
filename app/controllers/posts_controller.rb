a3e165e57a90a3cb5caec566560c1e879fcee90a
Megan Comeau <mlcomeau99@gmail.com>
2020-06-22 19:43:06 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,14 +6,8 @@ class PostsController < ApplicationControllerhelper_method :params
 
     # filter the @posts list based on user input
     if !params[:author].blank?
-      @posts = Post.where(author: params[:author])
+      @posts = Post.by_author(params[:author])
     elsif !params[:date].blank?
-      if params[:date] == "Today"
-        @posts = Post.where("created_at >=?", Time.zone.today.beginning_of_day)
-      else
-        @posts = Post.where("created_at <?", Time.zone.today.beginning_of_day)
-      end
-    else
       # if no filters are applied, show all posts
       @posts = Post.all
     end
